10312e595c92088a6f54dfda55b7e783f9946e5a
brekk <brekk@brekkbockrath.com>
2017-07-08 09:35:49 -0700

    added toString methods for pipe / compose

---
+++ src/fn.spec.js
@@ -0,0 +1,49 @@
+import test from 'ava'
+import {
+  curry,
+  PLACEHOLDER as $
+} from './curry'
+import {stringable, pipe, compose} from './fn'
+
+const add = curry((a, b) => a + b)
+const subtract = curry((a, b) => a - b)
+
+test(`pipe should make a composed function left to right`, (t) => {
+  t.plan(3)
+  t.is(typeof pipe, `function`)
+  const identityPipe = pipe(add($, 2), subtract($, 2))
+  t.is(identityPipe.toString(), `pipe(curry(function (a, b) {
+  return a + b;
+})(🍛,2),curry(function (a, b) {
+  return a - b;
+})(🍛,2))`)
+  const out = identityPipe(10)
+  t.is(out, 10)
+})
+
+test(`compose should make a composed function right to left`, (t) => {
+  t.plan(3)
+  t.is(typeof compose, `function`)
+  const identityPipe = compose(add($, 2), subtract($, 2))
+  t.is(identityPipe.toString(), `compose(curry(function (a, b) {
+  return a - b;
+})(🍛,2),curry(function (a, b) {
+  return a + b;
+})(🍛,2))`)
+  const out = identityPipe(10)
+  t.is(out, 10)
+})
+
+test(`stringable is a utility function`, (t) => {
+  t.plan(3)
+  t.is(typeof stringable, `function`)
+  const out = stringable(`xxx`, `abc`.split(``).map((x) => () => x))
+  t.is(typeof out, `function`)
+  t.is(out(), `xxx(function () {
+      return x;
+    },function () {
+      return x;
+    },function () {
+      return x;
+    })`)
+})
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 export * from './curry'
 export * from './curry-object'
 export * from './combinators'
-export * from './fn'
+import {pipe as p, compose as c} from './fn'
+
+export const pipe = p
+export const compose = c
