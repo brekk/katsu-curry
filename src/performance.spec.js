f06ac7b9030c123178a5b347c2380187ac8e5d6d
brekk <brekk@brekkbockrath.com>
2017-08-08 09:17:47 -0700

    yeah test it

--- src/performance.spec.js
+++ src/performance.spec.js
@@ -7,10 +7,13 @@ const getSpeed = (x) => parseFloat(x.split(` `)[4])
 // const oldSpeed = getSpeed(oldTime)
 // const newSpeed = getSpeed(newTime)
 const cwd = process.cwd()
+const {log: _log} = console
+const log = _log.bind(console)
 
 test.cb(`old should be faster than new`, (t) => {
   execa.shell(`${cwd}/node_modules/.bin/babel-node ${cwd}/src/performance.fixture.js`).then(
     (output) => {
+      log(output.stdout)
       const [oldSpeed, newSpeed] = output.stdout.split(`\n`)
       const a = getSpeed(oldSpeed)
       const b = getSpeed(newSpeed)
