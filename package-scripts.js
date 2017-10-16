8ab3f6fe57c85c8cf1548ecbda2835bf7642fee6
Brekk <brekk@brekkbockrath.com>
2017-10-16 09:43:21 -0700

    amend how babel publishing works (#23)

--- package-scripts.js
+++ package-scripts.js
@@ -1,3 +1,5 @@
 const germs = require(`germs`)
 const {name} = require(`./package.json`)
-module.exports = germs.build(name)
+module.exports = germs.build(name, {
+  build: `babel src -d . --ignore *.fixture.js,*.spec.js,index.js`
+})
