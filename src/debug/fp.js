3abf87df9ae6c7ee46c7ac13371869fc6d3b63c4
Brekk <brekk@brekkbockrath.com>
2018-02-11 10:13:38 -0800

    added errors to throw when bad data provided (#29)

--- src/debug/fp.js
+++ src/debug/fp.js
@@ -5,9 +5,22 @@ export {pipe} from '@fp/pipe'
 export {filter} from '@fp/filter'
 import {innerpipe} from '@fp/pipe'
 import {composedToString} from './to-string-composition'
+const slice = Array.prototype.slice
+const aintFunction = (x) => typeof x !== `function`
+
+const prepipe = (a, name = `pipe`) => {
+  const args = slice.call(a)
+  if (args.filter(aintFunction).length > 0) {
+    // eslint-disable-next-line fp/no-throw
+    throw new Error(
+      `${name} expected all arguments to be functions.`
+    )
+  }
+  return args
+}
 
 export function pipe() {
-  const args = Array.from(arguments)
+  const args = prepipe(arguments)
   const piped = innerpipe(args)
   // eslint-disable-next-line fp/no-mutation
   piped.toString = composedToString(args)
@@ -15,7 +28,7 @@ export function pipe() {
 }
 export function compose() {
   // eslint-disable-next-line fp/no-mutating-methods
-  const args = Array.from(arguments).reverse()
+  const args = prepipe(arguments, `compose`).reverse()
   const composed = innerpipe(args)
   // eslint-disable-next-line fp/no-mutation
   composed.toString = composedToString(args, `compose`)
